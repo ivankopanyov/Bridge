615f3494ffa9b2ad76642e5c0a8cd6ec9754576e
ivankopanyov <i.kopanyov@gmail.com>
2024-01-28 16:09:58 +0300

    Fix Bridge.Logger

--- src/Bridge.Logger/GlobaUsings.cs
+++ src/Bridge.Logger/GlobaUsings.cs
@@ -1,6 +1,6 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
 global using Serilog;
-global using Serilog.Templates;
+global using Serilog.Sinks.SystemConsole.Themes;
 
 global using LoggerExtensions = Bridge.Extensions.Logging.LoggerExtensions;
 
