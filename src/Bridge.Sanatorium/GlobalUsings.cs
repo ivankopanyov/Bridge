2b7d465b2a5b011c371b3c03f45b2cb92aafd62b
ivankopanyov <i.kopanyov@gmail.com>
2024-01-31 21:25:15 +0300

    Created Bridge.Sanatorium

---
+++ src/Bridge.Sanatorium/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Bridge.EventBus;
+global using Bridge.Logger;
+global using Bridge.Sanatorium.Handlers;
+global using Logus.HMS.Messages.Booking;
---
+++ src/Bridge.Sanatorium/Handlers/ReservationHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Bridge.Sanatorium.Handlers;
+
+internal class ReservationHandler(IEventBusService eventBusService, ILogger logger) 
+    : EventBus.EventHandler<ReservationUpdatedMessage>(eventBusService, logger)
+{
+    protected override string HandlerName => "N_SERVICE_BUS";
+
+    protected override Task HandleAsync(ReservationUpdatedMessage @in)
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override string? InputLog(ReservationUpdatedMessage @in) => @in.Id;
+}
